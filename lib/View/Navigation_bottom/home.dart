b643fb60b37e2e2f833521b2f39a1ebae5887a66
Shahab Mustafa <you@example.com>
2022-12-13 21:40:19 +0500

    update twitter clone

---
+++ lib/View/Navigation_bottom/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeNavigationBottom extends StatefulWidget {
+  const HomeNavigationBottom({Key? key}) : super(key: key);
+
+  @override
+  State<HomeNavigationBottom> createState() => _HomeNavigationBottomState();
+}
+
+class _HomeNavigationBottomState extends State<HomeNavigationBottom> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+         Text('data',)
+        ],
+      ),
+    );
+  }
+}
