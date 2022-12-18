8f23919baf3c3bf7fd1a5a97cec17de45c853813
Shahab <shahabmustafa57@gmail.com>
2022-12-18 22:07:51 +0500

    Update twitter Clone

--- lib/View/Splash_screen.dart
+++ lib/View/Splash_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:twitter_clone/View/Home_Screen.dart';
 import 'Auth/Login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,12 +12,17 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final _auth = FirebaseAuth.instance.currentUser;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     Timer(Duration(seconds: 3), () {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+      if(_auth == null){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+      }else{
+        Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+      }
     });
   }
   Widget build(BuildContext context) {
